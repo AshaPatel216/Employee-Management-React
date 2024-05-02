c79316997342cb935943eb72bfb921b23a7302b6
Asha Patel <asha@promactinfo.com>
2024-05-02 17:31:30 +0530

    Add basic header and content with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Container, Typography } from "@mui/material";
 
-function App() {
+const App = () => {
   return (
     <>
-      {/* Main section */}
-      <section className="App">
+      <section>
         {/* Header */}
-        <Header/>
+        <Header />
 
-        {/* Sidebar & content */}
-        <section>
-          {/* Sidebar */}
-          <nav></nav>
-
-          {/* Content */}
-          <section></section>
+        {/* Content */}
+        <section className="content">
+         <Container>
+         <Typography variant="h4" gutterBottom>
+            Welcome to My App
+          </Typography>
+          <Typography paragraph>
+            This is the content area of my application. You can put any content
+            here.
+          </Typography>
+         </Container>
         </section>
       </section>
     </>
   );
-}
+};
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
+import { Container } from "@mui/material"
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
 
 
 export const Header = () => {
   return (
-    <>
-    <header>
-      <h1>Header</h1>
-      <button className="btn btn-primary">Click</button>
-    </header>
-    </>
+    <AppBar position="fixed">
+      
+      <Toolbar>
+       <Container>
+       <Typography variant="h6" component="div">
+          Employee Management
+        </Typography>
+       </Container>
+       
+      </Toolbar>
+      
+     
+    </AppBar>
   )
 }
